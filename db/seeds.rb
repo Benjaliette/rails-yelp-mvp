08b05a0c087a3e693384b974359ec462802e1059
Benjamin Liet <liet.ben@gmail.com>
2022-05-18 12:16:56 +0200

    Made basic MVC yelp website - need to add style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Delete the database instances"
+Restaurant.destroy_all
+
+puts "Created 5 restaurants"
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save
+  5.times do
+    Review.create(
+      rating: [0, 1, 2, 3, 4, 5].sample,
+      content: Faker::Restaurant.review,
+      restaurant_id: restaurant.id
+    )
+  end
+end
